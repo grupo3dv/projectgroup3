1a4ee0a9d5f12c589a6c8786403e57dfca09cdb0
grupo3dv <107007884+grupo3dv@users.noreply.github.com>
2022-06-10 21:55:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -634,7 +634,7 @@ continents = [dict(label=continent ,value=continent_id) for continent, continent
 
 app = dash.Dash(__name__, 
              suppress_callback_exceptions=True,    
-             external_stylesheets=[dbc.themes.CYBORG],)
+             external_stylesheets=[dbc.themes.VAPOR],)
 
 server = app.server
 
